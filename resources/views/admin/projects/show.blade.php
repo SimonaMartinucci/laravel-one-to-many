9d89c646c0a120223c8f938206712c4d512a084f
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-09-25 16:05:49 +0200

    feat: add edit, delete, back buttons in projects.show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -10,10 +10,19 @@
     </div>
     @endif
 
-    <h1>{{ $project->title }}</h1>
+    <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning"><i class="fa-solid fa-pencil"></i></a>
+    <form action="{{ route('admin.projects.destroy', $project) }}" method="POST" class="d-inline" onsubmit="return confirm('Confermi l\'eliminazione del progetto {{ $project->name }}')">
+          @csrf
+          @method('DELETE')
+            <button type="submit" class="btn btn-danger">
+              <i class="fa-solid fa-trash"></i>
+            </button>
+          </form>
+    <h1 class="mt-3">{{ $project->title }}</h1>
     <h4>Categoria: {{ $project->category }}</h4>
     <h5>Tipo: {{ $project->type ? $project->type->name : 'NESSUN TIPO' }}</h5>
     <p>{{ $project->description }}</p>
+    <a href="{{ route('admin.projects.index') }}" class="btn btn-primary"><i class="fa-solid fa-arrow-left"></i></i></a>
 </div>
 
 @endsection
